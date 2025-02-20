cfcfc049e4d1e9359569991b1b0e950eeb044bfb
Haruya <hunter.hitter.08@gmail.com>
2025-02-20 15:15:23 +0900

    feat: DBにあるデータの取得機能を追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,10 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Todo;
+
 class TodoController extends Controller
 {
     public function index()
     {
+        $todo = new Todo();
+        $todos = $todo->all();  //allメソッドの返り値は、Illuminate\Database\Eloquent\Collectionクラスのインスタンス
+
         return view('todo.index');
     }
 }
